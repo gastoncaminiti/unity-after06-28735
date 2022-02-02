df31b53eafeced8dc1376c5f9e799ce42d2ec797
Gaston Caminiti <gastoncaminiti@gmail.com>
2022-02-01 22:22:09 -0300

    Code Live
    Movimiento y Rotacion con Axis
    Movimiento al rededor de

--- AFTER 06 - MOVIMIENTO/Assets/Scripts/EnemyController.cs
+++ AFTER 06 - MOVIMIENTO/Assets/Scripts/EnemyController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+   [SerializeField] private float speedEnemy;
+   [SerializeField] private GameObject rotationObject;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,10 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MoveAround(rotationObject);
+    }
+
+    private void MoveAround(GameObject AroundObject){
+        transform.RotateAround(AroundObject.transform.position,Vector3.up,speedEnemy * Time.deltaTime);
     }
 }
--- AFTER 06 - MOVIMIENTO/Assets/Scripts/PlayerController.cs
+++ AFTER 06 - MOVIMIENTO/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+   [SerializeField] private float speed;
+   [SerializeField] float speedRotation = 10f;
+
     // Start is called before the first frame update
+
     void Start()
     {
         
@@ -13,6 +17,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+        if (Input.GetKey(KeyCode.W))
+        {
+            MovePlayer(Vector3.forward);\
+
+        }
+        */
+        MovePlayerAxis();
+        RotatePlayerAxis();
     }
+
+    private void MovePlayer(Vector3 direction){
+        transform.Translate(speed * direction * Time.deltaTime);
+    }
+
+    private void MovePlayerAxis(){
+        float vAxis =Input.GetAxisRaw("Vertical");
+        Debug.Log(vAxis);
+        transform.Translate(speed * new Vector3(0,0,-vAxis) * Time.deltaTime);
+    }
+
+    private void RotatePlayerAxis(){
+        float hAxis =Input.GetAxisRaw("Horizontal");
+        Debug.Log(hAxis);
+        transform.Rotate(Vector3.up, hAxis * speedRotation * Time.deltaTime);
+       // transform.Translate(speed * new Vector3(0,0,haxis) * Time.deltaTime);
+    }
+
+
 }
